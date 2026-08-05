cf165920f144ef973805023d2fc235b8f5b12c64
OJS26 <oliverstrachan_01@hotmail.co.uk>
2026-08-05 15:56:42 +0100

    Add custom OLL and PLL reference images, replacing generated thumbnails

--- src/cases.ts
+++ src/cases.ts
@@ -1,3 +1,21 @@
+import antisuneImg from './assets/2-Look OLL/Antisune.png'
+import dotImg from './assets/2-Look OLL/Dot-shape.png'
+import ollHImg from './assets/2-Look OLL/H.png'
+import iShapeImg from './assets/2-Look OLL/I-shape.png'
+import lShapeImg from './assets/2-Look OLL/L-shape.png'
+import ollLImg from './assets/2-Look OLL/L.png'
+import piImg from './assets/2-Look OLL/Pi.png'
+import suneImg from './assets/2-Look OLL/Sune.png'
+import tImg from './assets/2-Look OLL/T.png'
+import uImg from './assets/2-Look OLL/U.png'
+
+import diagonalImg from './assets/2-Look PLL/Diagonal.png'
+import headlightsImg from './assets/2-Look PLL/Headlights.png'
+import pllHImg from './assets/2-Look PLL/PLL(H).png'
+import uaImg from './assets/2-Look PLL/PLL(Ua).png'
+import ubImg from './assets/2-Look PLL/PLL(Ub).png'
+import zImg from './assets/2-Look PLL/PLL(Z).png'
+
 export type Case = {
   id: string
   name: string
@@ -17,6 +35,7 @@ export const cases: Case[] = [
     part: 1,
     scramble: "f U R U' R' f' F U R U' R' F'",
     algorithm: "F R U R' U' F' f R U R' U' f'",
+    imageUrl: dotImg,
   },
   {
     id: 'oll-i',
@@ -25,6 +44,7 @@ export const cases: Case[] = [
     part: 1,
     scramble: "F U R U' R' F'",
     algorithm: "F R U R' U' F'",
+    imageUrl: iShapeImg,
   },
   {
     id: 'oll-l-edge',
@@ -33,6 +53,7 @@ export const cases: Case[] = [
     part: 1,
     scramble: "f U R U' R' f'",
     algorithm: "f R U R' U' f'",
+    imageUrl: lShapeImg,
   },
   {
     id: 'oll-sune',
@@ -41,6 +62,7 @@ export const cases: Case[] = [
     part: 2,
     scramble: "R U2 R' U' R U' R'",
     algorithm: "R U R' U R U2' R'",
+    imageUrl: suneImg,
   },
   {
     id: 'oll-antisune',
@@ -49,6 +71,7 @@ export const cases: Case[] = [
     part: 2,
     scramble: "R U R' U R U2' R'",
     algorithm: "R U2 R' U' R U' R'",
+    imageUrl: antisuneImg,
   },
   {
     id: 'oll-h',
@@ -57,6 +80,7 @@ export const cases: Case[] = [
     part: 2,
     scramble: "R U2 R' U' R U R' U' R U' R'",
     algorithm: "R U R' U R U' R' U R U2 R'",
+    imageUrl: ollHImg,
   },
   {
     id: 'oll-pi',
@@ -65,6 +89,7 @@ export const cases: Case[] = [
     part: 2,
     scramble: "R' U2 R2 U R2 U R2 U2 R'",
     algorithm: "R U2 R2 U' R2 U' R2 U2 R",
+    imageUrl: piImg,
   },
   {
     id: 'oll-u',
@@ -73,6 +98,7 @@ export const cases: Case[] = [
     part: 2,
     scramble: "R U2 R D R' U2 R D' R2",
     algorithm: "R2 D R' U2 R D' R' U2 R'",
+    imageUrl: uImg,
   },
   {
     id: 'oll-t',
@@ -81,6 +107,7 @@ export const cases: Case[] = [
     part: 2,
     scramble: "F R' F' r U R U' r'",
     algorithm: "r U R' U' r' F R F'",
+    imageUrl: tImg,
   },
   {
     id: 'oll-l',
@@ -89,6 +116,7 @@ export const cases: Case[] = [
     part: 2,
     scramble: "r U R' U' r' F R F'",
     algorithm: "F R' F' r U R U' r'",
+    imageUrl: ollLImg,
   },
   {
     id: 'pll-diagonal',
@@ -97,6 +125,7 @@ export const cases: Case[] = [
     part: 1,
     scramble: "F R' F' R U R U' R' F R U' R' U R U R' F'",
     algorithm: "F R U' R' U' R U R' F' R U R' U' R' F R F'",
+    imageUrl: diagonalImg,
   },
   {
     id: 'pll-headlights',
@@ -105,6 +134,7 @@ export const cases: Case[] = [
     part: 1,
     scramble: "F R U' R' U R U R2 F' R U R U' R'",
     algorithm: "R U R' U' R' F R2 U' R' U' R U R' F'",
+    imageUrl: headlightsImg,
   },
   {
     id: 'pll-ua',
@@ -113,6 +143,7 @@ export const cases: Case[] = [
     part: 2,
     scramble: "R2 U R U R' U' R' U' R' U R'",
     algorithm: "R U' R U R U R U' R' U' R2",
+    imageUrl: uaImg,
   },
   {
     id: 'pll-ub',
@@ -121,6 +152,7 @@ export const cases: Case[] = [
     part: 2,
     scramble: "R U' R U R U R U' R' U' R2",
     algorithm: "R2 U R U R' U' R' U' R' U R'",
+    imageUrl: ubImg,
   },
   {
     id: 'pll-h',
@@ -129,6 +161,7 @@ export const cases: Case[] = [
     part: 2,
     scramble: "M2 U' M2 U2 M2 U' M2",
     algorithm: "M2 U M2 U2 M2 U M2",
+    imageUrl: pllHImg,
   },
   {
     id: 'pll-z',
@@ -137,6 +170,7 @@ export const cases: Case[] = [
     part: 2,
     scramble: "M2 U2 M U' M2 U' M2 U' M",
     algorithm: "M' U M2 U M2 U M' U2 M2",
+    imageUrl: zImg,
   },
   {
     id: 'f2l-1',
